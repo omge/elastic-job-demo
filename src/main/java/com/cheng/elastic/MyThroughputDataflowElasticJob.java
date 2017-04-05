d450c285190573ea1610e4561b43f07d58c176e3
chengchenrui <380101538@qq.com>
2017-04-05 16:06:35 +0800

    优化

--- src/main/java/com/cheng/elastic/MyThroughputDataflowElasticJob.java
+++ src/main/java/com/cheng/elastic/MyThroughputDataflowElasticJob.java
@@ -1,5 +1,6 @@
 package com.cheng.elastic;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.cheng.elastic.pojo.Foo;
@@ -17,15 +18,22 @@ public class MyThroughputDataflowElasticJob implements DataflowJob<Foo> {
 
     public List<Foo> fetchData(ShardingContext shardingContext) {
         log.info("fetchData-->ShardingContext：{}", shardingContext.toString());
+        Foo foo = new Foo();
         switch (shardingContext.getShardingItem()) {
             case 0:
-                List<Foo> data = null;// get data from database by sharding item 0
+                List<Foo> data = new ArrayList<Foo>();// get data from database by sharding item 0
+                foo.setId("1");
+                data.add(foo);
                 return data;
             case 1:
-                List<Foo> data2 = null;// get data from database by sharding item 1
+                List<Foo> data2 = new ArrayList<Foo>();// get data from database by sharding item 1
+                foo.setId("2");
+                data2.add(foo);
                 return data2;
             case 2:
-                List<Foo> data3 = null;// get data from database by sharding item 2
+                List<Foo> data3 = new ArrayList<Foo>();// get data from database by sharding item 2
+                foo.setId("3");
+                data3.add(foo);
                 return data3;
             // case n: ...
         }
@@ -34,5 +42,6 @@ public class MyThroughputDataflowElasticJob implements DataflowJob<Foo> {
 
     public void processData(ShardingContext shardingContext, List<Foo> list) {
         log.info("processData-->ShardingContext：{}", shardingContext.toString());
+        log.info("List<Foo> list = {}", list.get(0));
     }
 }
--- src/main/java/com/cheng/elastic/pojo/Foo.java
+++ src/main/java/com/cheng/elastic/pojo/Foo.java
@@ -1,8 +1,18 @@
 package com.cheng.elastic.pojo;
 
+import java.io.Serializable;
+
+import lombok.Data;
+
 /**
  * @author chengchenrui
  * @version Id: Foo.java, v 0.1 2017.3.16 13:53 chengchenrui Exp $$
  */
-public class Foo {
+@Data
+public class Foo implements Serializable {
+
+    private static final long serialVersionUID = -2246495179426928344L;
+
+    private String            id;
+
 }
